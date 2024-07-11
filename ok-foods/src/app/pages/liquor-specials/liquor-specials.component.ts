c4b9662bba806eb3edd3d58de93877e595666daf
Pamela Lotriet <plotriet@retrorabbit.co.za>
2024-07-11 10:48:09 +0200

    Added carousel and conditional navbar depending on screen size

--- ok-foods/src/app/pages/liquor-specials/liquor-specials.component.ts
+++ ok-foods/src/app/pages/liquor-specials/liquor-specials.component.ts
@@ -3,16 +3,17 @@ import { BlobService } from '../../shared/services/blob-service';
 import { CardComponent } from '../../shared/components/card/card.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { environment } from '../../../envitonments/environment';
+import { CarouselComponent } from '../../shared/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-liquor-specials',
   standalone: true,
-  imports: [CardComponent, TranslateModule],
+  imports: [CardComponent, TranslateModule, CarouselComponent],
   templateUrl: './liquor-specials.component.html',
 })
 export class LiquorSpecialsComponent implements OnInit {
   azureBlobService = inject(BlobService);
-
+  viewAll = false;
   blobUrls: string[] = [];
 
   private alcUrl = environment.liquorSpecialsUrl;
@@ -24,5 +25,9 @@ export class LiquorSpecialsComponent implements OnInit {
       .subscribe((blobUrls) => {
         this.blobUrls = blobUrls;
       });
+  }
+
+  toggleViewAll() {
+    this.viewAll = !this.viewAll;
   }
 }
--- ok-foods/src/app/shared/components/navbar/navbar.component.ts
+++ ok-foods/src/app/shared/components/navbar/navbar.component.ts
@@ -1,16 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { SideBarComponent } from '../side-bar/side-bar.component';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [SideBarComponent],
+  imports: [SideBarComponent, TranslateModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   router = inject(Router);
+
+  isMobile = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    if (event && event.target) {
+      const width = (event.target as Window).innerWidth;
+      this.checkScreenSize(width);
+    }
+  }
+
+  constructor() {
+    this.checkScreenSize(window.innerWidth);
+  }
+
+  private checkScreenSize(width: number): void {
+    this.isMobile = width <= 768;
+  }
 
   routeHome() {
     this.router.navigate(['']);
